2169f0e93c2712d17454c1262bece7d836bafbbf
GiovanniDaldegan <71660160+GiovanniDaldegan@users.noreply.github.com>
2022-07-06 16:30:30 -0300

    resetCommands

--- deploy-commands.js
+++ deploy-commands.js
@@ -35,7 +35,7 @@ function registerCommands() {
 }
 
 function resetCommands(client) {
-	console.log(client.guilds.cache.get(guildId).commands);
+	console.log(client.guilds.cache.get(guildId).commands.forEach(command => {console.log(command)}));
 	/*
 	client.guilds.cache.get(guildId).commands.forEach(command => command.delete());
 	
